6d3b16b7c60fa9480e93e49f3e939e23eb865880
HungTrinhIT <minhhung.it.work@gmail.com>
2025-05-28 19:14:34 +0700

    add cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectToDB } from './configs/db.js';
 import appRouter from './routes/index.js';
+import cors from 'cors';
 
 dotenv.config();
 connectToDB();
@@ -10,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
